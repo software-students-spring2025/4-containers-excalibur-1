3f57b2af427d0dc086156e66f2434182bb432209
Echoudexigaigu <stevelinhaoxuan@gmail.com>
2025-04-10 17:16:00 +0800

    fix another bug

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -2,13 +2,16 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import pytest
 from app import app
-
 
 def test_upload_image():
     client = app.test_client()
-    fake_image = "data:image/jpeg;base64," + "aGVsbG8gd29ybGQ="  # base64 fake data
+
+    fake_image = (
+        "data:image/png;base64,"
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
+    )
+    
     response = client.post("/upload", json={"image": fake_image})
     data = response.get_json()
     
