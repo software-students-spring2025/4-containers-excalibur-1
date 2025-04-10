fe1ede1243ec56bbe22d844cc9701a88f0bc48cc
Echoudexigaigu <stevelinhaoxuan@gmail.com>
2025-04-10 17:02:09 +0800

    fix a bug

--- web-app/tests/test_db.py
+++ web-app/tests/test_db.py
@@ -4,9 +4,7 @@ from pymongo import MongoClient
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/")
 client = MongoClient(MONGO_URI)
 
-
 def test_mongodb_connection_write_and_cleanup():
-    client = MongoClient("mongodb://mongodb:27017/")
     db = client["facemoji"]
     collection = db["image_data"]
 
